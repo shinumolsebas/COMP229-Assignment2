283ded5096bd21a3822032ff325617c174d06994
Shinumol Sebastian <shinumolsebas@gmail.com>
2023-06-16 18:10:33 -0300

    update and delete added

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -4,7 +4,20 @@
 
     function start()
     {
-    console.log("App Started");
+    console.log("App Started..");
+
+    let deleteButtons = document.querySelectorAll('.btn-danger')
+
+    for(button of deleteButtons)
+    {
+        button.addEventListener('click',(event)=>{
+            if(!confirm("Are you sure to delete?"))
+            {
+                event.preventDefault();
+                window.location.assign('/business-list');
+            }
+        });
+    }
 
     }
     window.addEventListener("load", start);
--- server/routes/business.js
+++ server/routes/business.js
@@ -6,17 +6,79 @@ let mongoose =require('mongoose');
 
 let Business =require('../models/business');
 
-//Get route for business list page
+//Get route for business list page -- READ operation
 
 router.get('/', async(req,res,next)=>{
     try{
         let businessList = await Business.find();
        // console.log(businessList);
-        res.render('business', {title: 'Business List', BusinessList: businessList});
+        res.render('business/list', {title: 'Business', BusinessList: businessList});
     } 
     catch(err){
         console.log(err);
     }
 });
 
+// Get Route for th Add page-CREATE operation
+
+//Post Route for processing the Add page - CREATE operation
+
+//Get Route for displaying the Edit page -UPDATE operation
+
+router.get('/edit/:id', async(req,res,next)=>{
+    let id = req.params.id;
+
+    try{
+        let businessToEdit = await Business.findById(id);
+        res.render('business/edit',{title:'Edit Business', business:businessToEdit});
+    } catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+
+});
+//Post Route for processing the Edit page-UPDATE operation
+router.post('/edit/:id', async(req,res,next)=>{
+    let id = req.params.id;
+
+    let updatedBusiness={
+        "Name":req.body.name,
+        "Phone": req.body.phone,
+        "Email":req.body.email
+    };
+    try{
+        await Business.updateOne({_id:id}, updatedBusiness);
+        res.redirect('/business-list');
+    } catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+
+});
+
+router.get('/delete/:id', async(req,res,next)=>{
+    let id = req.params.id;
+    try{
+        await Business.findByIdAndRemove(id);
+        res.redirect('/business-list');
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+
+});
+
+
+//Get to confirm Deletion -DELETE operation
+router.get('/delete/:id', async(req,res,next)=>{
+    let id = req.params.id;
+    try{
+        await Business.findByIdAndRemove(id);
+        res.redirect('/business-list');
+    }catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+
+});
 module.exports=router;
