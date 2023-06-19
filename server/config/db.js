e4b145d409a0ca79a11ca834605054690b4b8c2f
Shinumol Sebastian <shinumolsebas@gmail.com>
2023-06-19 00:03:34 -0300

    db updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports=
 {
-   // "URI":"mongodb://127.0.0.1/business_contacts4"
+    //"URI":"mongodb://127.0.0.1/business_contacts4"
    
- "URI" : "mongodb+srv://shinumol27:JNCjaZLYAYp7guVD@cluster0.fajyrbq.mongodb.net/?retryWrites=true&w=majority"
+ "URI" : "mongodb+srv://shinumol27:GX59ODtBPAmnSTaf@cluster0.fajyrbq.mongodb.net/business_contacts4?retryWrites=true&w=majority"
 }
